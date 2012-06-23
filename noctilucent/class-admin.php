5180651dfe36dc70d483c234b152734413d2c127
Corey McKrill <corey@jupiterwise.com>
2012-06-22 17:48:27 -0700

    Minor tweak to admin class

--- noctilucent/class-admin.php
+++ noctilucent/class-admin.php
@@ -50,7 +50,7 @@ if ( ! class_exists( 'Noctilucent_Admin' ) ) {
             //remove_menu_page( 'index.php' );                // Dashboard
 			//remove_menu_page( 'edit.php' );                 // Posts
             //remove_menu_page( 'upload.php' );               // Media
-            //remove_menu_page( 'link-manager.php' );         // Links
+            remove_menu_page( 'link-manager.php' );           // Links
             //remove_menu_page( 'edit.php?post_type=page' );  // Pages
             //remove_menu_page( 'edit-comments.php' );        // Comments
             //remove_menu_page( 'themes.php' );               // Appearance
