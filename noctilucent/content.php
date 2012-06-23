10740dd36d979cc62846890ee29b0d4a2f45ca0c
Corey McKrill <corey@jupiterwise.com>
2012-06-22 17:53:26 -0700

    Make post meta more versatile

--- noctilucent/content.php
+++ noctilucent/content.php
@@ -1,7 +1,11 @@
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <header>
                         <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-                        <p>Posted on <time datetime="<?php the_time( 'c' ); ?>" pubdate="pubdate"><?php the_time('F j, Y'); ?></time> by <?php echo noctilucent_author_link(); ?><?php echo ( comments_open() ) ? ' | ' : ''; ( comments_open() ) ? comments_popup_link( 'No comments', '1 comment', '% comments', 'comments-link' ) : ''; ?><?php edit_post_link( 'Edit', ' | ', ''); ?></p>
+						
+						<span class="post-time"><time datetime="<?php the_time( 'c' ); ?>" pubdate><?php the_time('F j, Y'); ?></time></span>
+						<span class="post-author"><?php echo noctilucent_author_link(); ?></span>
+						<span class="post-comment-status"><?php ( comments_open() ) ? comments_popup_link( 'No comments', '1 comment', '% comments', 'comments-link' ) : ''; ?></span>
+						<?php edit_post_link( 'Edit', ' | ', ''); ?>
                     </header>
                     <?php the_content( 'Continue reading &ldquo;' . the_title( '', '', false ) . '&rdquo; &raquo;' ); ?>
                     <footer>
@@ -12,11 +16,9 @@
                         // Post Meta
                         if ( has_category() || has_tag() ) : ?>
 
-                        <p class="postmetadata">
-                            <?php echo ( has_category() ) ? 'Posted in: ' : ''; the_category( ', ' ); ?><br />
-                            <?php the_tags(); ?>
+						<span class="post-categories"><?php echo ( has_category() ) ? 'Categories: ' : ''; the_category( ', ' ); ?></span>
+						<span class="post-tags"><?php the_tags(); ?></span>
 
-                        </p>
                         <?php endif; ?>
 
                     </footer>
