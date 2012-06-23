0ead07ca59ad15c7e11f8d384414013e6c88afa3
Corey McKrill <corey@jupiterwise.com>
2012-06-22 18:09:55 -0700

    Moving existing styles and images to child theme

---
+++ child-theme/config.rb
@@ -0,0 +1,16 @@
+# Additional Compass plugins and import paths:
+require 'ninesixty'
+require 'compass-h5bp'
+
+# Configuration settings:
+http_path = "/"
+relative_assets = true
+css_dir = "css"
+sass_dir = "src"
+images_dir = "img"
+javascripts_dir = "js"
+
+# Output settings: :development || :production
+environment = :development
+output_style = (environment == :production) ? :compressed : :expanded
+line_comments = (environment == :production) ? false : true
