c2d9b6376f47586274e98e321790be26c23687b7
Corey McKrill <corey@jupiterwise.com>
2012-08-29 20:48:55 -0700

    Moved section header logic to functions.php

--- noctilucent/loop.php
+++ noctilucent/loop.php
@@ -1,30 +1,11 @@
             <section class="content">
 <?php
 // Section header for archive pages
-$nocti_archive_title = '';
-if ( is_category() ) {
-    $nocti_archive_title = 'Archive for &ldquo;' . single_cat_title( '', false ) . '&rdquo;';
-} elseif ( is_tag() ) {
-    $nocti_archive_title = 'Posts tagged &ldquo;' . single_tag_title( '', false ) . '&rdquo;';
-} elseif ( is_day() ) {
-    $nocti_archive_title = 'Archive for ' . get_the_time('F jS, Y');
-} elseif ( is_month() ) {
-    $nocti_archive_title = 'Archive for ' . get_the_time('F, Y');
-} elseif ( is_year() ) {
-    $nocti_archive_title = 'Archive for ' . get_the_time('Y');
-} elseif ( is_author() ) {
-    $nocti_curauth = get_user_by( 'slug', get_query_var('author_name') );
-    $nocti_archive_title = 'Posts by ' . $nocti_curauth->display_name;
-} elseif ( is_search() ) {
-    global $wp_query;
-    $nocti_search_count = $wp_query->found_posts;
-    $nocti_search_label = ( $nocti_search_count == 1 ) ? 'result' : 'results';
-    $nocti_archive_title = $nocti_search_count . ' search ' . $nocti_search_label . ' for &ldquo;' . get_search_query() . '&rdquo;';
-}
+$nocti_archive_title = noctilucent_section_header();
 if ( $nocti_archive_title != '' ) : ?>
 
                 <header>
-                    <h1><?php echo $nocti_archive_title; ?></h1>
+                    <h3><?php echo $nocti_archive_title; ?></h3>
                 </header>
 <?php endif;
 
