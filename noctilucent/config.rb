d033553445f019eebcfb2dfb1512b3918371af83
Corey McKrill <corey@jupiterwise.com>
2012-08-29 20:41:41 -0700

    Stylesheet overhaul.

--- noctilucent/config.rb
+++ noctilucent/config.rb
@@ -1,16 +1,16 @@
 # Additional Compass plugins and import paths:
-require 'ninesixty'
-add_import_path "C:/Users/Corey/Documents/Web Design/Projects/Compass"
+require 'susy'
+require 'compass-h5bp'
 
 # Configuration settings:
-http_path = "/"
+http_path       = "/"
 relative_assets = true
-css_dir = "css"
-sass_dir = "src"
-images_dir = "img"
+css_dir         = "css"
+sass_dir        = "src"
+images_dir      = "img"
 javascripts_dir = "js"
 
 # Output settings: :development || :production
-environment = :development
-output_style = (environment == :production) ? :compressed : :expanded
-line_comments = (environment == :production) ? false : true
+environment     = :development
+output_style    = ( environment == :production ) ? :compressed : :expanded
+line_comments   = ( environment == :production ) ? false : true
