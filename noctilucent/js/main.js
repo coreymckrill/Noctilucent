13d4c74121705ec7a86cb8c01d9aa89f78c7d994
Corey McKrill <corey@jupiterwise.com>
2012-08-29 21:11:38 -0700

    Add more selectors to apply Baseline.js to

--- noctilucent/js/main.js
+++ noctilucent/js/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
 	
 	// Fix vertical rhythm for editor-inserted images
 	// Uses baseline.js script in plugins.js
-	$('img.aligncenter, img.alignnone, .aligncenter img, .gallery-item img')
+	$('img.alignleft, img.alignright, img.aligncenter, img.alignnone, .aligncenter img, .gallery-item img')
 		.baseline(24)
 		.attr('height','')
 		.attr('width','')
