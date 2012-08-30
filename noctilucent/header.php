d85906f956c39d9a07bece8cfaf3fe0f3b5cddf1
Corey McKrill <corey@jupiterwise.com>
2012-08-29 21:05:14 -0700

    Minor h5bp updates

--- noctilucent/header.php
+++ noctilucent/header.php
@@ -27,9 +27,9 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<?php // Prompt IE 6 users to install Chrome Frame. Remove this if you support IE 6.
-		  // chromium.org/developers/how-tos/chrome-frame-getting-started ?>
-	<!--[if lt IE 7]><p class="chromeframe">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
+    <!--[if lt IE 7]>
+	<p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+	<![endif]-->
 
 	<header id="header-banner" role="banner">
 		<hgroup>
