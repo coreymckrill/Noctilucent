d7534a4b96ff6b0c673882ed7cfe7e45570b37f9
Corey McKrill <corey@jupiterwise.com>
2012-08-29 20:50:58 -0700

    Initiate baseline and tinynav

--- noctilucent/js/main.js
+++ noctilucent/js/main.js
@@ -1,26 +1,16 @@
-/* Author: 
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+jQuery(document).ready(function($){
+	
+	// Fix vertical rhythm for editor-inserted images
+	// Uses baseline.js script in plugins.js
+	$('img.aligncenter, img.alignnone, .aligncenter img, .gallery-item img')
+		.baseline(24)
+		.attr('height','')
+		.attr('width','')
+	;
+	
+	// Turn primary nav into dropdown on small screens
+	// Uses tinynav.js script in plugins.js
+	$("#nav-primary > ul").tinyNav({header:true});
+	
+});
