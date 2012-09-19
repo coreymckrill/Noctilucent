c8b74efb26ccaa1fb2bac98e88b06fdb0e5e565b
Corey McKrill <corey@jupiterwise.com>
2012-09-19 13:17:37 -0700

    Fixed: baseline-resized images not showing in IE

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ jQuery(document).ready(function($){
 	// Uses baseline.js script in plugins.js
 	$('img.alignleft, img.alignright, img.aligncenter, img.alignnone, .aligncenter img, .gallery-item img')
 		.baseline(24)
-		.attr('height','')
-		.attr('width','')
+		.removeAttr('height')
+		.removeAttr('width')
 	;
 	
 	// Turn primary nav into dropdown on small screens
