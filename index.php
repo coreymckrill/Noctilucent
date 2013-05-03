5aae1c16d8f2fcaa638cbd7ad6374fbc80309a8e
Corey McKrill <corey@jupiterwise.com>
2013-05-02 21:00:07 -0700

    Add php header statement above header include to fix IE 404 issue

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php get_header(); ?>
+<?php
+global $is_IE;
+if ( is_404() && $is_IE ) {
+	ob_start();
+	header( "HTTP/1.1 404 Not Found" );
+}
+?>
+
+<?php get_header( apply_filters( 'noctilucent_header_template', null ) ); ?>
 
 <?php get_template_part( 'loop', apply_filters( 'noctilucent_loop_template', 'index' ) ); ?>
 
