7cd11ef677fe57d22ac192e07b4ec43e6a40da89
Corey McKrill <corey@jupiterwise.com>
2013-05-02 20:41:45 -0700

    Minor tweaks to 404 HTML

--- content-404.php
+++ content-404.php
@@ -2,7 +2,9 @@
                     <header class="post-header">
                         <h1 class="post-title">Error 404: Not Found</h1>
                     </header>
-                    <p>Unfortunately, nothing exists at this address: <em><?php echo noctilucent_get_protocol() . '//' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?></em></p>
-                    <p><?php get_search_form(); ?></p>
-                    <p><?php wp_tag_cloud(); ?></p>
+	                <div class="post-content">
+	                    <p>Unfortunately, nothing exists at this address: <em><?php echo noctilucent_get_protocol() . '//' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; ?></em></p>
+	                    <?php get_search_form(); ?>
+	                    <div class="tagcloud"><?php wp_tag_cloud(); ?></div>
+		            </div>
                 </article>
